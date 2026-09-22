6b6accc86efb35c5a97f00b6ec4b3880de617e67
christianocj <christianojulio007@gmail.com>
2026-09-22 10:18:19 +0100

    configura o DI, pipeline e Cloudflare AI service"

--- TextToImageGenerator/Program.cs
+++ TextToImageGenerator/Program.cs
@@ -1,26 +1,26 @@
 using TextToImageGenerator.Components;
+using TextToImageGenerator.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.Configure<CloudflareAiOptions>(builder.Configuration.GetSection("CloudflareAi"));
+builder.Services.AddHttpClient<ICloudflareAiService, CloudflareAiService>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseExceptionHandler("/Error");
     app.UseHsts();
 }
-app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
-app.UseHttpsRedirection();
 
+app.UseHttpsRedirection();
+app.UseStaticFiles();
 app.UseAntiforgery();
 
-app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
